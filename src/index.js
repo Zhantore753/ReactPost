7cb084e982590fa40eb02e523c50890968a2d13f
Zhantore <753gg@mail.ru>
2020-11-01 19:08:14 +0600

    Start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+
+const Header = () => {
+  return <h2>Hello World!</h2>
+}
+
+const Field = () =>{
+  const holder = "Enter here";
+  const styledField = {
+    width: '300px'
+  }
+
+  return <input 
+    style={styledField}
+    type="text" 
+    placeholder={holder}
+    autoComplete="" 
+    className="first" 
+    htmlFor="" />
+}
+
+const Btn = () =>{
+  const text = "Log in";
+  const logged = false;
+  return <button>{logged ? "Enter" : text}</button>
+}
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Field/>
+      <Btn/>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
