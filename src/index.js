fa34610a9cf76c401ff43c361818eaab420976ee
Zhantore <753gg@mail.ru>
2020-11-01 20:37:18 +0600

    Структура

--- src/index.js
+++ src/index.js
@@ -1,45 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './components/app/';
 
-const Header = () => {
-  return <h2>Hello World!</h2>
-}
-
-const Field = () =>{
-  const holder = "Enter here";
-  const styledField = {
-    width: '300px'
-  }
-
-  return <input 
-    style={styledField}
-    type="text" 
-    placeholder={holder}
-    autoComplete="" 
-    className="first" 
-    htmlFor="" />
-}
-
-const Btn = () =>{
-  const text = "Log in";
-  const logged = false;
-  return <button>{logged ? "Enter" : text}</button>
-}
-
-const App = () => {
-  return (
-    <div>
-      <Header/>
-      <Field/>
-      <Btn/>
-    </div>
-  )
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App/>, document.getElementById('root'));
