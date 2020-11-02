1d58d58f0302e9e448afb8883cf70ae749fa046e
Zhantore <753gg@mail.ru>
2020-11-02 19:32:20 +0600

    styled-component подключен

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,7 +5,14 @@ import SearchPanel from '../search-panel/';
 import PostStatusFilter from '../post-status-filter/';
 import PostList from '../post-list/';
 import PostAddForm from '../post-add-form/';
+
 import './app.css';
+import styled from 'styled-components';
+
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`;
 
 const App = () => {
 
@@ -16,7 +23,7 @@ const App = () => {
     ];
 
     return(
-        <div className="app">
+        <AppBlock>
             <AppHeader/>
             <div className="search-panel d-flex">
                 <SearchPanel/>
@@ -24,7 +31,7 @@ const App = () => {
             </div>
             <PostList posts={data}/>
             <PostAddForm/>
-        </div>
+        </AppBlock>
     )
 };
 
